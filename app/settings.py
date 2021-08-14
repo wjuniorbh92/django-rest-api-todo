273f6c36e3508e75cb3a0adf13e26a99b3ccfccb
wjuniorbh92 <68206230+wjuniorbh92@users.noreply.github.com>
2021-08-14 15:16:19 -0300

    Add django_heroku

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication'
     ]
 }
+
+django_heroku.settings(locals())
