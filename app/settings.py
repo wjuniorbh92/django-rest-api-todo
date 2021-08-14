353270e1c53ad7e98b4d2b8b9f9aab427d51fae1
wjuniorbh92 <68206230+wjuniorbh92@users.noreply.github.com>
2021-08-14 15:18:21 -0300

    change migrate

--- app/settings.py
+++ app/settings.py
@@ -117,9 +117,8 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
